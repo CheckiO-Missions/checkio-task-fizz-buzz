b76df375cb19f594fc97710662cb4ba24ad98371
Oleksandr Zozulia <freemanlex@gmail.com>
2022-08-04 10:57:17 +0300

    next-api, uk hints

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -1,41 +1,6 @@
-//Dont change it
-requirejs(['ext_editor_io', 'jquery_190'],
+requirejs(['ext_editor_io2', 'jquery_190'],
     function (extIO, $) {
-        var $tryit;
-
-        var io = new extIO({
-            functions: {
-                js: 'fizzBuzz',
-                python: 'checkio'
-            },
-            tryit:function (this_e) {
-                $tryit = this_e.extSetHtmlTryIt(this_e.getTemplate('tryit')).find(".tryit-content");
-                $tryit.find('.bn-check').click(function (e) {
-                    e.preventDefault();
-                    var $input = $tryit.find(".tool .input-number");
-                    var data = Number($input.val());
-                    if (!isNaN(data)) {
-                        data = Number(data);
-                    }
-                    this_e.extSendToConsoleCheckiO(data);
-                    e.stopPropagation();
-                    return false;
-                });
-
-                $tryit.find('.bn-random').click(function (e) {
-                    e.preventDefault();
-                    var numb = Math.floor(Math.random() * 10000);
-                    $tryit.find(".tool .input-number").val(numb);
-                    return false;
-                });
-
-            },
-            retConsole: function (ret) {
-                $tryit.find(".checkio-result").html("Your Result<br>" + ret);
-            }
-        });
+        var io = new extIO({});
         io.start();
-
-
     }
 );
